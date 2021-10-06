a5d15f72598480059b3bc901262706457d7e7185
Jonathon Marolf <jmarolf@microsoft.com>
2021-10-05 18:57:54 -0700

    Move RegisterInstance to its own method so MSBuild assemblies are not loaded

--- src/Tools/IdeCoreBenchmarks/FindReferencesBenchmarks.cs
+++ src/Tools/IdeCoreBenchmarks/FindReferencesBenchmarks.cs
@@ -16,6 +16,7 @@ using AnalyzerRunner;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Diagnosers;
 using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.FindSymbols;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Host.Mef;
@@ -27,85 +28,103 @@ namespace IdeCoreBenchmarks
     [MemoryDiagnoser]
     public class FindReferencesBenchmarks
     {
-        [Benchmark]
-        public async Task RunFindReferences()
+        MSBuildWorkspace _workspace;
+        Solution _solution;
+        ISymbol _type;
+
+        [GlobalSetup]
+        public void Setup()
         {
-            try
-            {
-                // QueryVisualStudioInstances returns Visual Studio installations on .NET Framework, and .NET Core SDK
-                // installations on .NET Core. We use the one with the most recent version.
-                var msBuildInstance = MSBuildLocator.QueryVisualStudioInstances().OrderByDescending(x => x.Version).First();
+            SetUpWorkspace();
+            LoadSolution().Wait();
+        }
 
-                MSBuildLocator.RegisterInstance(msBuildInstance);
+        private static void SetUpWorkspace()
+        {
+            // QueryVisualStudioInstances returns Visual Studio installations on .NET Framework, and .NET Core SDK
+            // installations on .NET Core. We use the one with the most recent version.
+            var msBuildInstance = MSBuildLocator.QueryVisualStudioInstances().OrderByDescending(x => x.Version).First();
 
-                var roslynRoot = Environment.GetEnvironmentVariable(Program.RoslynRootPathEnvVariableName);
-                var solutionPath = Path.Combine(roslynRoot, @"C:\github\roslyn\Compilers.sln");
+            MSBuildLocator.RegisterInstance(msBuildInstance);
+        }
 
-                if (!File.Exists(solutionPath))
-                    throw new ArgumentException("Couldn't find Compilers.sln");
+        private async Task LoadSolution()
+        {
+            var roslynRoot = Environment.GetEnvironmentVariable(Program.RoslynRootPathEnvVariableName);
+            var solutionPath = Path.Combine(roslynRoot, @"C:\source\dotnet\roslyn\Compilers.sln");
 
-                Console.Write("Found Compilers.sln: " + Process.GetCurrentProcess().Id);
+            if (!File.Exists(solutionPath))
+                throw new ArgumentException("Couldn't find Compilers.sln");
 
-                var assemblies = MSBuildMefHostServices.DefaultAssemblies
-                    .Add(typeof(AnalyzerRunnerHelper).Assembly)
-                    .Add(typeof(FindReferencesBenchmarks).Assembly);
-                var services = MefHostServices.Create(assemblies);
+            Console.Write("Found Compilers.sln: " + Process.GetCurrentProcess().Id);
 
-                var workspace = MSBuildWorkspace.Create(new Dictionary<string, string>
+            var assemblies = MSBuildMefHostServices.DefaultAssemblies
+                .Add(typeof(AnalyzerRunnerHelper).Assembly)
+                .Add(typeof(FindReferencesBenchmarks).Assembly);
+            var services = MefHostServices.Create(assemblies);
+
+            _workspace = MSBuildWorkspace.Create(new Dictionary<string, string>
                 {
                     // Use the latest language version to force the full set of available analyzers to run on the project.
                     { "LangVersion", "9.0" },
                 }, services);
 
-                if (workspace == null)
-                    throw new ArgumentException("Couldn't create workspace");
+            if (_workspace == null)
+                throw new ArgumentException("Couldn't create workspace");
 
-                workspace.TryApplyChanges(workspace.CurrentSolution.WithOptions(workspace.Options
-                    .WithChangedOption(StorageOptions.Database, StorageDatabase.SQLite)));
+            _workspace.TryApplyChanges(_workspace.CurrentSolution.WithOptions(_workspace.Options
+                .WithChangedOption(StorageOptions.Database, StorageDatabase.SQLite)));
 
-                Console.WriteLine("Opening roslyn.  Attach to: " + Process.GetCurrentProcess().Id);
+            Console.WriteLine("Opening roslyn.  Attach to: " + Process.GetCurrentProcess().Id);
 
-                var start = DateTime.Now;
-                var solution = workspace.OpenSolutionAsync(solutionPath, progress: null, CancellationToken.None).Result;
-                Console.WriteLine("Finished opening roslyn: " + (DateTime.Now - start));
+            var start = DateTime.Now;
+            _solution = await _workspace.OpenSolutionAsync(solutionPath, progress: null, CancellationToken.None);
+            Console.WriteLine("Finished opening roslyn: " + (DateTime.Now - start));
 
-                // Force a storage instance to be created.  This makes it simple to go examine it prior to any operations we
-                // perform, including seeing how big the initial string table is.
-                var storageService = workspace.Services.GetPersistentStorageService(workspace.CurrentSolution.Options);
-                if (storageService == null)
-                    throw new ArgumentException("Couldn't get storage service");
+            // Force a storage instance to be created.  This makes it simple to go examine it prior to any operations we
+            // perform, including seeing how big the initial string table is.
+            var storageService = _workspace.Services.GetPersistentStorageService(_workspace.CurrentSolution.Options);
+            if (storageService == null)
+                throw new ArgumentException("Couldn't get storage service");
 
-                using (var storage = await storageService.GetStorageAsync(SolutionKey.ToSolutionKey(workspace.CurrentSolution), checkBranchId: true, CancellationToken.None))
-                {
-                    Console.WriteLine();
-                }
-
-                // There might be multiple projects with this name.  That's ok.  FAR goes and finds all the linked-projects
-                // anyways  to perform the search on all the equivalent symbols from them.  So the end perf cost is the
-                // same.
-                var project = solution.Projects.First(p => p.AssemblyName == "Microsoft.CodeAnalysis");
-
-                start = DateTime.Now;
-                var compilation = await project.GetCompilationAsync();
-                Console.WriteLine("Time to get first compilation: " + (DateTime.Now - start));
-                var type = compilation.GetTypeByMetadataName("Microsoft.CodeAnalysis.SyntaxToken");
-                if (type == null)
-                    throw new Exception("Couldn't find type");
-
-                Console.WriteLine("Starting find-refs");
-                start = DateTime.Now;
-                var references = await SymbolFinder.FindReferencesAsync(type, solution);
-                Console.WriteLine("Time to find-refs: " + (DateTime.Now - start));
-                var refList = references.ToList();
-                Console.WriteLine($"References count: {refList.Count}");
-                var locations = refList.SelectMany(r => r.Locations).ToList();
-                Console.WriteLine($"Locations count: {locations.Count}");
-            }
-            catch (ReflectionTypeLoadException ex)
+            using (var storage = await storageService.GetStorageAsync(SolutionKey.ToSolutionKey(_workspace.CurrentSolution), checkBranchId: true, CancellationToken.None))
             {
-                foreach (var ex2 in ex.LoaderExceptions)
-                    Console.WriteLine(ex2);
+                Console.WriteLine();
             }
+
+            // There might be multiple projects with this name.  That's ok.  FAR goes and finds all the linked-projects
+            // anyways  to perform the search on all the equivalent symbols from them.  So the end perf cost is the
+            // same.
+            var project = _solution.Projects.First(p => p.AssemblyName == "Microsoft.CodeAnalysis");
+
+            start = DateTime.Now;
+            var compilation = await project.GetCompilationAsync();
+            Console.WriteLine("Time to get first compilation: " + (DateTime.Now - start));
+            _type = compilation.GetTypeByMetadataName("Microsoft.CodeAnalysis.SyntaxToken");
+            if (_type == null)
+                throw new Exception("Couldn't find type");
+        }
+
+        [Benchmark]
+        public async Task RunFindReferences()
+        {
+            Console.WriteLine("Starting find-refs");
+            var start = DateTime.Now;
+            var references = await SymbolFinder.FindReferencesAsync(_type, _solution);
+            Console.WriteLine("Time to find-refs: " + (DateTime.Now - start));
+            var refList = references.ToList();
+            Console.WriteLine($"References count: {refList.Count}");
+            var locations = refList.SelectMany(r => r.Locations).ToList();
+            Console.WriteLine($"Locations count: {locations.Count}");
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            _workspace?.Dispose();
+            _workspace = null;
+            _solution = null;
+            _type = null;
         }
     }
 }
