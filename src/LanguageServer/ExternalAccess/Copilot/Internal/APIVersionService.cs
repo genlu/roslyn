79699b8653c1ede932d11b91e64551f27c7fcc7a
gel@microsoft.com <gel@microsoft.com>
2025-03-17 17:25:31 -0700

    Add a request handler that provide EA API version

---
+++ src/LanguageServer/ExternalAccess/Copilot/Internal/APIVersionService.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Composition;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.LanguageServer.Handler;
+
+namespace Microsoft.CodeAnalysis.LanguageServer.ExternalAccess.Copilot.Internal;
+
+[Shared]
+[Method("roslyn/copilot/getExternalAccessVersion")]
+[ExportCSharpVisualBasicStatelessLspService(typeof(CopilotAPIVersionRequestHandler))]
+internal sealed class CopilotAPIVersionRequestHandler : ILspServiceRequestHandler<CopilotAPIVersionRequest, int>
+{
+    private const int CurrentVersion = 0;
+
+    [ImportingConstructor]
+    [Obsolete("This exported object must be obtained through the MEF export provider.", error: true)]
+    public CopilotAPIVersionRequestHandler()
+    {
+    }
+
+    public bool MutatesSolutionState => false;
+
+    public bool RequiresLSPSolution => false;
+
+    public Task<int> HandleRequestAsync(CopilotAPIVersionRequest param, RequestContext context, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(CurrentVersion);
+    }
+}
+
+internal sealed class CopilotAPIVersionRequest
+{
+    [JsonPropertyName("extensionVersion")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? ExtensionVersion { get; set; }
+}
